b42678e914d2296f0dc157258d89459d2fabe7a5
Brandon Ngene <brandonngene19@gmail.com>
2025-09-27 14:14:20 -0400

    Add files via upload

---
+++ CONNECT 4 GAME/game.js
@@ -0,0 +1,137 @@
+var player1 = prompt("Player One: Enter Your Name , you will be Blue");
+var player1Color = 'rgb(254, 228, 64)';
+
+var player2 = prompt("Player Two: Enter Your Name, you will be Red");
+var player2Color = 'rgb(255, 107, 107)';
+
+var game_on = true;
+var table = $('table tr');
+
+function reportWin(row,col) {
+  console.log("You won starting at this row,col");
+  console.log(row);
+  console.log(col);
+}
+
+function changeColor(rowIndex,colIndex,color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
+
+function returnColor(rowIndex,colIndex) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+function checkBottom(colIndex) {
+  var colorReport = returnColor(6,colIndex);
+  for (var row = 6; row > -1; row--) {
+    colorReport = returnColor(row,colIndex);
+    if (colorReport === 'rgb(128, 128, 128)') {
+      return row
+    }
+  }
+}
+
+function colorMatchCheck(one,two,three,four){
+  return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
+}
+
+// Win Checks
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (let col = 0; col < 7; col++) {
+    for (let row = 0; row < 3; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (let col = 0; col < 5; col++) {
+    for (let row = 0; row < 7; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Game End
+function gameEnd(winningPlayer) {
+  for (let col = 0; col < 7; col++) {
+    for (let row = 0; row < 7; row++) {
+      $('h3').fadeOut('3000');
+      $('h6').fadeOut('3000');
+      $('h1').text(winningPlayer+" has won! Refresh your browser to play again!")
+    }
+  }
+}
+
+
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
+
+// Start with Player One
+$('h6').text(player1+": it is your turn, please pick a column to drop your chip.");
+
+$('.board button').on('click',function() {
+
+  // Recognize what column was chosen
+  var col = $(this).closest("td").index();
+
+  // Get back bottom available row to change
+  var bottomAvail = checkBottom(col);
+
+  // Drop the chip in that column at the bottomAvail Row
+  changeColor(bottomAvail,col,currentColor);
+
+  // Check for a win or a tie.
+  if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+    gameEnd(currentName);
+  }
+
+  // If no win or tie, continue to next player
+  currentPlayer = currentPlayer * -1 ;
+
+  // Re-Check who the current Player is.
+  if (currentPlayer === 1) {
+    currentName = player1;
+    $('h6').text(currentName+": it is your turn, please pick a column to drop your chip.");
+    currentColor = player1Color;
+  }else {
+    currentName = player2
+    $('h3').text(currentName+": it is your turn, please pick a column to drop your chip.");
+    currentColor = player2Color;
+  }
+
+})
